b6f8331e917d1ef1b47fafda66bda060c5c686f7
procodingclass <88050205+procodingclass@users.noreply.github.com>
2023-07-14 10:02:41 +0530

    Add files via upload

---
+++ TA1/app.py
@@ -0,0 +1,14 @@
+# Import flask framework
+
+
+# Createa a new flask app
+
+
+# Create a route "/"
+
+
+# Define home() trigger function taht returns a string "Welcome to Block Jinja"
+
+
+# Check if __name___ == '__main__' and run the app i.e server
+
